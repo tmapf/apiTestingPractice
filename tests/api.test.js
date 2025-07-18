14533f7e139a907b06f486307ff65b35a0a70216
ArseniMahulenka <Arseni.Mahulenka@sobleg.com>
2025-07-18 15:43:49 +0200

    youtube comment get request

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,5 +1,30 @@
-import {test, expect} from "@playwright/test";
+import {test, expect, request} from "@playwright/test";
 
-test('api get request', async ({request}) => {
-    const response = await request.get(`http://localhost:3000/api`);
+const VIDEO_ID = 'XEZYadc2o-8';
+
+
+
+
+test.only('api get request', async () => {
+
+    const baseURL = 'https://www.googleapis.com/youtube/v3'
+    const context = await request.newContext();
+
+    const response = await context.get(`${baseURL}/commentThreads`, {
+        params: {
+            part: 'snippet',
+            videoId: VIDEO_ID,
+            key: API_TOKEN,
+            maxResults: 5,
+        }
+    });
+
+    expect(response.ok()).toBeTruthy();
+
+    const data = await response.json();
+    for (const item of data.items) {
+        const comment = item.snippet.topLevelComment.snippet.textDisplay;
+        console.log(comment + '\n');
+
+    }
 })
